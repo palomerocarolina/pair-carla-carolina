34a9b253a553439f813dbd0cc2c55edd22ed2225
palomerocarolina <palomero.carolina@gmail.com>
2024-07-01 11:34:28 +0200

    update_database

--- pair5-db-creation.sql
+++ pair5-db-creation.sql
@@ -30,8 +30,6 @@ CREATE TABLE Empleados (
 	fecha_incorporacion DATE NOT NULL,
 	PRIMARY KEY (id_empleado)
 	);
-    
-DROP TABLE Facturas;
 
 CREATE TABLE Facturas (
 		id_factura INT AUTO_INCREMENT NOT NULL,
@@ -58,6 +56,39 @@ CREATE TABLE Facturas (
             ON DELETE RESTRICT
     );
     
+ALTER TABLE Zapatillas 
+	ADD COLUMN marca VARCHAR(45) NOT NULL,
+    ADD COLUMN talla INT NOT NULL;
 
+ALTER TABLE Empleados
+	ADD salario FLOAT NOT NULL;
+    
+ALTER TABLE Clientes
+	DROP COLUMN pair;
+    
+ALTER TABLE Facturas
+	ADD COLUMN Total FLOAT;
+    
+INSERT INTO Zapatillas (modelo, color, marca, talla)
+	VALUES ("XQYUN", "Negro", "Nike", 42),
+    ("UOPMN", "Rosas", "Nike", 39),
+    ("OPNYT", "Verdes", "Adidas", 35);
+
+INSERT INTO Empleados (nombre, tienda, salario, fecha_incorporacion)
+	VALUES ("Laura", "Alcobendas", 25987, "2010-09-03"),
+    ("Ester", "Oviedo", 30165.98, "2000-11-29"),
+    ("Maria", "Sevilla", NULL, "2001-04-11");
+
+INSERT INTO Clientes (nombre, num_telefono, email, direccion, ciudad, provincia, codigo_postal)
+	VALUES ("Monica", "1234567289", "monica@email.com", "Calle Felicidad", "Móstoles", "Madrid", 28176),
+    ("Carmen", "298463759", "carmen@email.com", "Calle del Color", "Vigo", "Pontevedra", 23456);
+
+INSERT INTO Facturas (id_factura, numero_factura, fecha, id_zapatilla, id_empleado, id_cliente, total)
+	VALUES ("123", "2001-12-11", 1, 2. 1, 54.98),
+    ("1234", "2005-05-23", 1, 1, 3, 89.91),
+    ("12345", "2015-09-18, 2, 3, 3, 76.23);
+    
+
+	
 
  
